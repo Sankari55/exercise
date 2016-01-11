45ec60a4d17469f166623672b10a37aa031b2bb8
sankariSS <sankari@chargebee.com>
2016-01-11 16:09:26 +0530

    Abc changes

--- Abc.java
+++ Abc.java
@@ -1,24 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-class Abc{
+class Abc  {
 	public static void main(String args[])
 	{
 	 Scanner in = new Scanner(System.in);
-	 String abc;
+	 String abc,sentence="";
 	 char z;
-	 int s,sum=0,i;
+	 int s,sum=0;
 	 System.out.println("Enter the string:");
 	 abc=in.nextLine();
-	 for(i=0;i<abc.length();i++)
+	 for(int i=0;i<abc.length();i++){
+	 	if(Character.isUpperCase(abc.charAt(i))==true){
+	 		char ch2=(char)(abc.charAt(i)+32);
+	 		sentence=sentence+ch2;
+	 	}
+	 	else if(Character.isLowerCase(abc.charAt(i))==true){
+	 		char ch2=(char)(abc.charAt(i));
+	 		sentence=sentence+ch2;
+	 	}
+	 	else{
+	 		System.out.println("Enter valid input.");
+	 		return;
+	 	}
+	 }
+	 for(int i=0;i<sentence.length();i++)
 	 {
-        if(i==0) System.out.print("(");
-	z=abc.charAt(i);
+        if(i==0){ System.out.print("(");  }
+	    z=sentence.charAt(i);
         s=(int)z;
         System.out.print(s-96);
-	sum+=s-96;
-	if(i==abc.length()-1) System.out.print("="+sum+")");
-	else if(i!=abc.length()) System.out.print("+");
+	    sum+=s-96;
+	    if(i==sentence.length()-1) { System.out.print("="+sum+")"); }
+	    else if(i!=sentence.length()){ System.out.print("+"); }
 	 }
 	 if(sum%2!=0) System.out.println("odd");
 	 else System.out.println("even");
